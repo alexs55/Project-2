ab984bf6d63f0492d5b5261e38c1bd65aab7c7fc
alexs55 <alex.acosta.dv@gmail.com>
2019-04-06 13:47:08 -0500

    adding the google maps js functionality

--- public/js/index.js
+++ public/js/index.js
@@ -99,6 +99,158 @@ var handleDeleteBtnClick = function() {
   });
 };
 
+var userLocation = {};
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+    console.log(userLocation);
+  } else {
+    console.log("Your browser does not support Geolocation");
+  }
+}
+function showPosition(position) {
+  var latitude = position.coords.latitude;
+  var longitude = position.coords.longitude;
+  userLocation.userLatitude = latitude;
+  userLocation.userLongitude = longitude;
+
+  if (userLocation.userLatitude === latitude) {
+    googleApiCall();
+  }
+}
+getLocation();
+function googleApiCall() {
+  console.log("making api call");
+  var url =
+    "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?";
+  var apiKey = "AIzaSyCUM6ziq10bpobC1rqrO3O9LGJwgzUTJEA";
+  var combinedLocation =
+    userLocation.userLatitude + "," + userLocation.userLongitude;
+
+  console.log("Returned Lat and Long is " + combinedLocation);
+  $.ajax(url, {
+    data: {
+      key: apiKey,
+      location: combinedLocation,
+      radius: 10000
+      // 'keyword': userSelection.subCategorySelection,
+      // 'name': userSelection.typeSelection,
+      // 'opennow': true,
+      // 'rankby': 'distance',
+    }
+  }).then(function(response) {
+    console.log("API call received");
+    console.log(JSON.parse(JSON.stringify(response)));
+    googleMap();
+  });
+}
+
+function googleMap() {
+  var map;
+
+  //    starting directions services
+  var postArray = [];
+
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: userLocation.userLatitude, lng: userLocation.userLongitude },
+    zoom: 18
+  });
+
+  var postDisplay = new google.maps.InfoWindow();
+
+  //    updates the content of the map
+  infoWindow = new google.maps.InfoWindow();
+
+  // structure of the db
+  var result = {
+    post: [
+      {
+        body: {
+          title: "the master",
+          text: "my name is alex"
+        },
+        latitude: 30.38727219999997,
+        longitude: -97.726508
+      },
+      {
+        body: {
+          title: "the master",
+          text: "my name is alex"
+        },
+        latitude: 30.267153,
+        longitude: -97.743057
+      },
+      {
+        body: {
+          title: "the master",
+          text: "my name is alex"
+        },
+        latitude: 30.387272199999995,
+        longitude: -95.726508
+      },
+      {
+        body: {
+          title: "the master",
+          text: "my name is alex"
+        },
+        latitude: 30.387272199999994,
+        longitude: -94.726508
+      },
+      {
+        body: {
+          title: "the master",
+          text: "my name is alex"
+        },
+        latitude: 30.387272199999993,
+        longitude: -97.726508
+      }
+    ]
+  };
+
+  showSteps(result, postArray, postDisplay, map);
+  function showSteps(result, postArray, postDisplay, map) {
+    // For each step, place a marker, and add the text to the marker's infowindow.
+    // Also attach the marker to an array so we can keep track of it and remove it
+    // when calculating new routes.
+
+    // this will be the template fo google maps API to render the table parameters
+    var listPost = result.post;
+
+    console.log(listPost[0].body);
+    for (var i = 0; i < listPost.length; i++) {
+      myLatlng = new google.maps.LatLng(
+        listPost[i].latitude,
+        listPost[i].longitude
+      );
+      var post = (postArray[i] = postArray[i] || new google.maps.Marker());
+      post.setMap(map);
+      console.log("hey");
+      post.setPosition(myLatlng);
+      attachInstructionText(
+        postDisplay,
+        post,
+        listPost[i].body.title,
+        listPost[i].body.text,
+        map
+      );
+      //   body meant to be the post object that includes the name and the text
+    }
+  }
+
+  function attachInstructionText(postDisplay, post, title, text, map) {
+    console.log("hello");
+
+    google.maps.event.addListener(post, "click", function() {
+      // Open an info window when the marker is clicked on, containing the text
+      // of the step.
+      postDisplay.setContent(
+        "<h1>" + title + "</h1>" + "<br>" + "<p>" + text + "</p>"
+      );
+      postDisplay.open(map, post);
+    });
+  }
+}
+
 // Add event listeners to the submit and delete buttons
 $submitBtn.on("click", handleFormSubmit);
 $exampleList.on("click", ".delete", handleDeleteBtnClick);
