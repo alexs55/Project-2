26b80306c39d85417503ca1835677a4db5964161
Carmen Lucero Sandoval <carmenlucero786@gmail.com>
2019-04-06 14:17:23 -0500

    updated api routes

--- public/js/index.js
+++ public/js/index.js
@@ -7,59 +7,70 @@ var $submitBtn = $("#submit");
 var $exampleList = $("#example-list");
 
 // The API object contains methods for each kind of request we'll make
+var post = {
+  // subject: $("#subject").val().trim(),
+  post: "I am waiting for true love and my bus.",
+  latitude: 30.287648,
+  longitude: -97.727912
+};
+
 var API = {
-  saveExample: function(example) {
+  createPost: function(post) {
     return $.ajax({
       headers: {
         "Content-Type": "application/json"
       },
       type: "POST",
-      url: "api/examples",
-      data: JSON.stringify(example)
+      url: "api/posts",
+      data: JSON.stringify(post)
     });
   },
-  getExamples: function() {
+  getPosts: function() {
     return $.ajax({
-      url: "api/examples",
+      url: "api/posts",
       type: "GET"
     });
-  },
-  deleteExample: function(id) {
-    return $.ajax({
-      url: "api/examples/" + id,
-      type: "DELETE"
-    });
   }
+  // deleteExample: function(id) {
+  //   return $.ajax({
+  //     url: "api/examples/" + id,
+  //     type: "DELETE"
+  //   });
+  // }
 };
+
+API.getPosts(post).then(function(response) {
+  console.log(response);
+});
 
 // refreshExamples gets new examples from the db and repopulates the list
-var refreshExamples = function() {
-  API.getExamples().then(function(data) {
-    var $examples = data.map(function(example) {
-      var $a = $("<a>")
-        .text(example.text)
-        .attr("href", "/example/" + example.id);
+// var refreshExamples = function() {
+//   API.getExamples().then(function(data) {
+//     var $examples = data.map(function(example) {
+//       var $a = $("<a>")
+//         .text(example.text)
+//         .attr("href", "/example/" + example.id);
 
-      var $li = $("<li>")
-        .attr({
-          class: "list-group-item",
-          "data-id": example.id
-        })
-        .append($a);
+//       var $li = $("<li>")
+//         .attr({
+//           class: "list-group-item",
+//           "data-id": example.id
+//         })
+//         .append($a);
 
-      var $button = $("<button>")
-        .addClass("btn btn-danger float-right delete")
-        .text("ｘ");
+//       var $button = $("<button>")
+//         .addClass("btn btn-danger float-right delete")
+//         .text("ｘ");
 
-      $li.append($button);
+//       $li.append($button);
 
-      return $li;
-    });
+//       return $li;
+//     });
 
-    $exampleList.empty();
-    $exampleList.append($examples);
-  });
-};
+//     $exampleList.empty();
+//     $exampleList.append($examples);
+//   });
+// };
 
 // handleFormSubmit is called whenever we submit a new example
 // Save the new example to the db and refresh the list
@@ -67,7 +78,7 @@ var handleFormSubmit = function(event) {
   event.preventDefault();
   //grab user's input value & store it in a variable in an object format
   //change example to user
-  var example = {
+  var post = {
     name: $userName.val().trim(),
     subject: $subject.val().trim(),
     inputText: $userPost.val().trim()
@@ -78,7 +89,7 @@ var handleFormSubmit = function(event) {
     return;
   }
 
-  API.saveExample(example).then(function() {
+  API.createPost(post).then(function() {
     refreshExamples();
   });
   //clear input fields
